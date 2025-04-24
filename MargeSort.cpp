981ad692d8c05f5d9bb53bc08a28b0c6c44cef2b
vaniaazhariaputri <vaniaazharia@gmail.com>
2025-04-24 10:41:36 +0700

    memasukkan panjang element array

--- MargeSort.cpp
+++ MargeSort.cpp
@@ -1,2 +1,25 @@
 #include <iostream>
 using namespace std;
+
+// create main array & temporary array
+int arr[20], B[20];
+// n is array input size
+int n;
+
+void input() 
+{
+    while (true) 
+    {
+        cout <<"Masukkan Panjang element array : ";
+        cin >> n;
+
+        if (n <= 20)
+        {
+            break;
+        }
+        else
+        {
+            cout << "\nMaksimal panjang array adalah 20";
+        }
+    }
+    
