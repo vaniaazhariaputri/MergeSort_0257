006a6b5eb100aec8e093fffa3cdd22af4361bbed
vaniaazhariaputri <vaniaazharia@gmail.com>
2025-04-24 10:50:38 +0700

    memasukkan function margeSort dan step

--- MargeSort.cpp
+++ MargeSort.cpp
@@ -22,7 +22,6 @@ void input()
             cout << "\nMaksimal panjang array adalah 20";
         }
     }
-
     cout << "\n-----------------------" << endl;
     cout << "\nInput Isi element array" << endl;
     cout << "\n-----------------------" << endl;
@@ -33,5 +32,42 @@ void input()
         cin >> arr[i];
     }
 }
+
+// create function mergeSort
+void mergeSort(int low, int high) 
+{
+
+    if (low >= high ) //step 1
+    {
+        return; //step 1.a
+    }
+
+    int mid = (low + high ) / 2; //step 2
+
+    //step 3
+    // fungsi rekursi - memanggil diri sendiri
+    mergeSort(low, mid);      //step 3.a
+    mergeSort(mid + 1, high); //step 3.b
+
+    //step 4
+    int i = low;    //step 4.a
+    int j = mid + 1; //step 4.b
+    int k = low; //step 4.c
+
+    while(i <= mid && j <= high)  //step 4.d
+    {
+        if(arr[i] <=  arr[j])  //step 4.d.i
+        {
+            B[k] = arr[i];
+            i++;
+        }
+        else
+        {
+            B[k] = arr[j];
+            j++;
+        }
+        k++; // step 4.d.ii
+    }
+
 
     
